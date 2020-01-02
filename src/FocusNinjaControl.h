fe929b55f0cff07d4667c1c42151f54f86dceecf
Erik Reitsma <developer@ernovation.com>
2020-01-02 22:46:44 +0100

    Create some fields and buttons.

--- src/FocusNinjaControl.h
+++ src/FocusNinjaControl.h
@@ -1,13 +1,47 @@
-#define PIN_DIRECTION           19
-#define PIN_STEP                18
-#define PIN_ENDSTOP             32
-#define PIN_CAMERA_RELEASE      21
+#ifndef FOCUSNINJACONTROL_H
+#define FOCUSNINJACONTROL_H
 
-#define MICROSTEPPING           4
-#define DEGREES_PER_STEP        1.8
-#define MILIMETER_PER_ROTATION  2
-#define DEFAULT_SPEED           2       //omwentelingen per seconde
-#define MAX_POSITION            130     //maximale positie in mm
+#define PIN_DIRECTION 19
+#define PIN_STEP 18
+#define PIN_ENDSTOP 32
+#define PIN_CAMERA_RELEASE 21
+
+#define MICROSTEPPING 4
+#define DEGREES_PER_STEP 1.8
+#define MILLIMETER_PER_ROTATION 2
+#define DEFAULT_SPEED 2  //omwentelingen per seconde
+#define MAX_POSITION 130 //maximale positie in mm
 #define FORWARDS false
 #define BACKWARDS true
 
+class FocusNinjaControl
+{
+public:
+    bool homed = false;
+    float position = 0;
+
+    //Initialization code
+    FocusNinjaControl();
+    void moveCarriage(float millimeter, bool direction);
+    //Home the carriage
+    void homeCarriage();
+    bool isMoving();
+    void stop();
+    void releaseShutter();
+    //This needs to be called from the loop
+    void motorControl();
+    void setLogger(void (*fun_ptr)(const char*));
+    void log(const char*);
+
+private:
+    float mmPerStep = (2 * DEGREES_PER_STEP) / (360 * MICROSTEPPING);
+    int carriageDirection = 0;
+    int numberOfPulses = 0;
+    int pulseWidthMs = (1000000 / (2 * DEGREES_PER_STEP * 360 / DEGREES_PER_STEP)) / DEFAULT_SPEED;
+    void (*logger)(const char*) = 0;
+
+    //Basic function to rotate the steppermotor
+    void rotateStepper(float degrees, int rotationSpeed, bool rotationDirection);
+    void reportPosition(float pos);
+};
+#endif
